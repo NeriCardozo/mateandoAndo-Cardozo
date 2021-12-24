911ac4d504b1a4cc2cd90171a7a3fe719e0895f7
NeriCardozo <75796427+NeriCardozo@users.noreply.github.com>
2021-12-24 19:37:50 -0300

    Agregando detalles

---
+++ mateando-ando-cardozo/src/data/categoryList.js
@@ -0,0 +1,23 @@
+
+    
+    const categoryList = [
+        {
+    categoryId: 1,
+    categoryName: "Mates"
+    },
+    {
+    categoryId: 2,
+    categoryName: "Bombillas"
+    },
+        {
+    categoryId: 3,
+    categoryName: "Yerbas"
+        },
+    {
+    categoryId: 4,
+    categoryName: "Accesorios"
+        },
+                
+    ];
+
+    export default categoryList;
